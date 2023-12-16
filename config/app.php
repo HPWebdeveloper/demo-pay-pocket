1a28df4bfbf7a20c9d1d90e9d5049d4e3c6b4cac
Hamed Panjeh <panjeh@gmail.com>
2023-12-16 13:26:35 -0300

    Remove superfluous service providers.

--- config/app.php
+++ config/app.php
@@ -163,10 +163,6 @@ return [
         /*
          * Application Service Providers...
          */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
     ])->toArray(),
 
